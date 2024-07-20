da81e0827ef978a9b4c21098fc03acad616f09aa
Matteo Porrani <m.porrani@gmail.com>
2024-07-20 18:14:17 +0200

    Add router & Pinia

--- App.vue
+++ App.vue
@@ -1,14 +1,10 @@
 <template>
-	<main class="h-100 bg-sky-200 p-4">
+	<main>
 
-		<h1 class="text-5xl">The App</h1>
-
-		<HelloCompo/>
+		<router-view/>
 
 	</main>
 </template>
 
 <script setup>
-import HelloCompo from "@/components/HelloCompo.vue"
-
 </script>
---
+++ views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+
+	<h2>Home View</h2>
+
+</template>
