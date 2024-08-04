1289dcbeffa5a1201b52f55d36874e12bf35f1be
Matteo Porrani <m.porrani@gmail.com>
2024-08-04 21:36:59 +0200

    Improve error handling on getLinkedEntityName

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -9,14 +9,15 @@ export const getLinkedEntityName = (entityName, relation) => {
 		"link",
 	];
 	
+	// guard on invalid entityName
+	if (!e.includes(entityName)) return null;
+	// guard on invalid relation
 	if (!['parent', 'child'].includes(relation)) return null;
 	
 	if (relation === 'parent') {
     return e[e.indexOf(entityName) - 1];
   } else if (relation === 'child') {
     return e[e.indexOf(entityName) + 1];
-  } else {
-    return null;
   }
-	
+
 }
