c03012d6ac8a87a007f08dd6bff84726acfa14c3
Matteo Porrani <m.porrani@gmail.com>
2024-08-15 15:32:59 +0200

    Set slate as primary color

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ import {createApp} from "vue";
 import {createPinia} from "pinia";
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import {definePreset} from "@primevue/themes";
 // import Lara from '@primevue/themes/lara';
 // import Nora from '@primevue/themes/nora';
 
@@ -18,10 +19,29 @@ const app = createApp(App);
 const pinia = createPinia();
 app.use(router);
 app.use(pinia);
+
+const MyPreset = definePreset(Aura, {
+	semantic: {
+		primary: {
+			50: '{slate.50}',
+			100: '{slate.100}',
+			200: '{slate.200}',
+			300: '{slate.300}',
+			400: '{slate.400}',
+			500: '{slate.500}',
+			600: '{slate.600}',
+			700: '{slate.700}',
+			800: '{slate.800}',
+			900: '{slate.900}',
+		}
+	}
+})
+
 app.use(PrimeVue, {
 	// unstyled: true
 	theme: {
-		preset: Aura,
+		// preset: Aura,
+		preset: MyPreset,
 	}
 });
 
