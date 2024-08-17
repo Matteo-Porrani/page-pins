d9885825a8396df0dede37ae18ce611fb03e9e3b
Matteo Porrani <m.porrani@gmail.com>
2024-08-18 00:14:38 +0200

    Update v to 0.2.4

--- src/components/info/InfoAbout.vue
+++ src/components/info/InfoAbout.vue
@@ -1,6 +1,4 @@
 <script setup>
-
-
 </script>
 
 <template>
@@ -9,10 +7,10 @@
 
 	<h2 class="flex items-center justify-center gap-2 mb-16">
 		<span class="text-lg font-bold text-slate-700">PagePins</span>
-		<pre class="text-sm bg-slate-800 text-white rounded-md py-1 px-4">v0.2.3</pre>
+		<pre class="text-sm bg-slate-800 text-white rounded-md py-1 px-4">v0.2.4</pre>
 	</h2>
 
-	<div class="bg-white rounded-md text-slate-600 w-[60%] flex flex-col gap-4 py-4 px-6 mx-auto">
+	<div class="bg-white rounded-md leading-6 text-sm text-slate-600 w-[90%] xl:w-[70ch] flex flex-col gap-4 py-8 px-6 mx-auto">
 		<p>PagePins is a lightweight web app designed to <b>organize your favorite website bookmarks</b>, built with <b>Vue 3</b>, <b>Tailwind CSS</b>, and the <b>Prime Vue</b> UI Suite.</p>
 
 		<p>The main workspace features <b>categories</b> to help you group bookmarks by general themes like "Movies", "Programming" or "Sports".</p>
@@ -21,7 +19,7 @@
 
 		<p>All your <b>data is stored locally</b> in the browser, nothing is sent to remote servers, ensuring your privacy. To transfer your bookmarks between devices, simply use the <b>Import/Export</b> feature to save your data to a .txt file.</p>
 
-		<b class="text-lg">Happy pinning!</b>
+		<b>Happy pinning!</b>
 	</div>
 
 </template>
